e618c0d2bd7057492328cda03685ddc4e5001942
bchau1000 <bchau1000@gmail.com>
2021-03-27 12:27:19 -0700

    Initialized ReactJS and ExpressJS environment

---
+++ webserver/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+})
