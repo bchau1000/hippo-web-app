f44fd20e2d0d8efa955afb5bb52ed8229306fb74
bchau1000 <bchau1000@gmail.com>
2021-03-27 12:46:04 -0700

    Finalizing folder structure

---
+++ app/src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="navbar-container">
+                <ul>
+                    <li><a href="#">Study Buddy</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
