4ab5d89652e675677c723111902eea01499c309a
bchau1000 <bchau1000@gmail.com>
2021-03-31 23:08:58 -0700

    Added addition/removal of flashcards for Create Study Sets

--- app/src/components/createSet/createSet.js
+++ app/src/components/createSet/createSet.js
@@ -1,16 +1,49 @@
 import React from 'react';
-
+import AddIcon from '@material-ui/icons/Add';
 import CardForm from "./cardForm/cardForm.js";
 import "./createSet.css";
 
 class CreateSet extends React.Component {
     
-    deleteCard(cardNum) {
-        console.log("Deleting card: " + cardNum);
+    constructor(props) {
+        super(props);
+        this.state = {
+            flash_cards: [0, 0, 0, 0],
+        }
     }
 
-    clearCard(cardNum) {
-        console.log("Clearing card: " + cardNum);
+    // front is a bool, true inserts a card to the front of the list, false appends a card to the back of the list
+    addCard(front) {
+        let new_flash_cards = this.state.flash_cards.slice();
+
+        if(front) {
+            new_flash_cards.unshift(0);
+
+        }
+        else {
+            new_flash_cards.push(0);
+        }
+
+        this.setState({
+            flash_cards: new_flash_cards,
+        });
+    }
+
+    deleteCard = (index) => {
+        if(this.state.flash_cards.length > 1) {
+            let new_flash_cards = this.state.flash_cards.slice();
+        
+            new_flash_cards.splice(index, 1);
+
+            this.setState({
+                flash_cards: new_flash_cards,
+        });
+        }
+        
+    }
+
+    clearCard(index) {
+        console.log("Clearing card: " + index);
     }
 
     render() {
@@ -30,11 +63,27 @@ class CreateSet extends React.Component {
                         <div className="field-label">DESCRIPTION</div>
                     </div>
                 </form>
-                <div className="center-container">
-                    <CardForm delete={this.deleteCard} clear={this.clearCard} cardNum="1"></CardForm>
-                    <CardForm delete={this.deleteCard} clear={this.clearCard} cardNum="2"></CardForm>
-                    <CardForm delete={this.deleteCard} clear={this.clearCard} cardNum="3"></CardForm>
+
+                <div className="add-card-container" onClick={() => this.addCard(true)}>
+                        <div className="add-card">
+                            <AddIcon className="add-card-icon"></AddIcon>
+                            <div className="add-card-text">Add a card</div>
+                        </div>
                 </div>
+
+                <div className="center-container">
+                    {this.state.flash_cards.map((_, index) => {
+                        return <CardForm delete={this.deleteCard} clear={this.clearCard} cardNum={index}></CardForm>;
+                    })}
+                </div>
+                
+                <div className="add-card-container" onClick={() => this.addCard(false)}>
+                    <div className="add-card">
+                        <AddIcon className="add-card-icon"></AddIcon>
+                        <div className="add-card-text">Add a card</div>
+                    </div>
+                </div>
+
                 <div className="submit-container"></div>
             </div>
         )
@@ -42,3 +91,7 @@ class CreateSet extends React.Component {
 }
 
 export default CreateSet;
+
+
+
+// <CardForm delete={this.deleteCard} clear={this.clearCard} cardNum="1"></CardForm>
